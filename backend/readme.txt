94282b2d4bfaec40219822d5b81c47a7c8a86b2e
maheggh <111765195+maheggh@users.noreply.github.com>
2024-04-06 15:11:18 +0200

    Delete backend directory

--- backend/readme.txt
+++
@@ -1,1 +0,0 @@
-The backend project goes in this folder
--- backend/routes/assessment.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const assessmentController = require('../controllers/assessmentController');
-
-router.get('/', assessmentController.getAllAssessments);
-router.get('/search', assessmentController.search);
-
-module.exports = router;
