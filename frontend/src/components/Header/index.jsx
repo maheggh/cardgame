38e40787a1821a13c46d835d1dd8057601fb4eac
VegardTheMonkey <vegardstamadsen@gmail.com>
2024-04-10 03:20:30 +0200

    fixed header background color

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -8,28 +8,30 @@ const Header = ({ isLoggedIn }) => {
     const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
 
     return (
-        <header className="super-assessor-header">
-            <a href='/' className="logo">
-                <img src={logo} alt="Logo"/>
-            </a>
-            {!isAuthPage && (
-                <nav>
-                    {isLoggedIn ? (
-                        <>
-                            <a href="/dashboard" className="link">Dashboard</a>
-                            <a href="/cards" className="link">Cards</a>
-                            <a href="/users" className="link">Users</a>
-                            <a href="/logout" className="link">Log out</a>
-                        </>
-                    ) : (
-                        <>
-                            <a href="/login" className="link nav-button">Login</a>
-                            <a href="/signup" className="link nav-button">Sign up</a>
-                        </>
-                    )}
-                </nav>
-            )}
-        </header>
+        <div class="super-assessor-header-container">
+            <header className="super-assessor-header">
+                <a href='/' className="logo">
+                    <img src={logo} alt="Logo"/>
+                </a>
+                {!isAuthPage && (
+                    <nav>
+                        {isLoggedIn ? (
+                            <>
+                                <a href="/dashboard" className="link">Dashboard</a>
+                                <a href="/cards" className="link">Cards</a>
+                                <a href="/users" className="link">Users</a>
+                                <a href="/logout" className="link">Log out</a>
+                            </>
+                        ) : (
+                            <>
+                                <a href="/login" className="link nav-button">Login</a>
+                                <a href="/signup" className="link nav-button">Sign up</a>
+                            </>
+                        )}
+                    </nav>
+                )}
+            </header>
+        </div>
     );
 };
 
