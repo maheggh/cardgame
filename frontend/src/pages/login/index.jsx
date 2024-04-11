6902485a9e0d5776603972da1370a41ffd3da920
VegardTheMonkey <vegardstamadsen@gmail.com>
2024-04-11 04:39:51 +0200

    some comments and polishing code

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -5,19 +5,26 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../UserContext.jsx';
 
 function Login() {
+    // State for email and password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    // Get login function from context and navigation function
     const { loginAuth } = useAuth();
     const navigate = useNavigate();
 
+    // Handle email and password input changes
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     };
+
+
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     };
 
+    // handle login form
     const handleLogin = async (event) => {
         event.preventDefault();
         try {
