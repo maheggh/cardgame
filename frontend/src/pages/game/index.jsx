70cea158c8894921d669c241af360523b4c228e8
VegardTheMonkey <vegardstamadsen@gmail.com>
2024-04-27 16:09:39 +0200

    pulls cards from db in game

--- frontend/src/pages/game/index.jsx
+++ frontend/src/pages/game/index.jsx
@@ -1,172 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './style.css';
-import { login, authorize } from '../../api/api.js';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../UserContext.jsx';
-import SUPERMissionCard from './../../components/CardHTMLComponent/SUPER-mission-card.js';
-import SUPERAssessmentCard from './../../components/CardHTMLComponent/SUPER-assessment-card.js';
 
 function Game() {
-    const { loginAuth, token } = useAuth();
-    const cardOrder = ["Who is assessed", "The assessor", "Assessment artefact", "Assessment format", "Context", "Assessment timing"];
-    const navigate = useNavigate();
-    const [cardInfo, setCardInfo] = useState([]);
-    const [favCards, setFavCards] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const missionCount = 3;
-    const assessmentCount = 6;
-
-        const getAllCards = async () => {
-            try {
-              const allCards = await authorize(token);
-                setCardInfo(allCards);
-                setLoading(true);
-            if (cardInfo.length > 0) {
-                    populateAssessments(assessmentCount);
-                    populateMissions(missionCount);
-            }
-            } catch (err) {
-                console.log(err);
-            }
-        }
-
-        function getLocalStorage(){
-            var x = localStorage.getItem("FAVOURITE_CARDS_LIST_STORE");
-               setFavCards(JSON.parse(x));
-        }
-
-
-useEffect(() => {
-    var x = localStorage.getItem("FAVOURITE_CARDS_LIST_STORE");
-    
-}, [favCards]);
-
     useEffect(() => {
-        getAllCards()
-        const copiedObj = JSON.parse(JSON.stringify(cardInfo));
-        getLocalStorage();
-    }, [loading])
-
-
-        function getRandomItem(list){
-            if ( list == undefined || list == null) return null;
-            var randomIndex = Math.floor(Math.random() * list.length);
-            const object = list[randomIndex];
-            // remove card
-            list.splice(randomIndex, 1);
-            //console.log(list);
-            return object;
-        }
-
-        function randomizeCards(){
-            const copiedObj = JSON.parse(JSON.stringify(obj));
-
-        }
-
-        document.addEventListener("favourited", (e) => {
-            const combinedArray = cardInfo;
-            const foundCard = combinedArray.find(item => item['card-id'] == e.detail.cardId);
-            console.log(foundCard);
-            if(e.detail.isFav === true){addToFavouritesList(foundCard)}
-            else if(e.detail.isFav === false){removeFromFavouritesList(foundCard)}
-        });
-
-        //creates each mission card as a custom component with randomly selected data from the database, then appends it to the document
-        function populateMissions(count){
-            const missionList = document.getElementById("mission-list");
-            missionList.innerHTML = '';
-            for (let i = 0; i < count; i++) {
-                var missionItem;
-                const missionObj = (cardInfo.length > 0) ? (getRandomItem(cardInfo.filter(card => card["card-type"] === "Mission"))) : ('') ;
-                missionItem = document.createElement('super-mission-card');
-                //sets each pair from the JSON as an attribute in the HTML
-                var x;
-                for (x in missionObj) {
-
-                    missionItem.setAttribute(x, missionObj[x]);
-                }
-
-                //lastly, append to page
-                missionList.appendChild(missionItem);
-            }         
-        }
-
-        function populateAssessments(count){
-            //define what the assessments will be added to
-            const selectedCards = {};
-            const assessmentList = document.getElementById("assessment-list");
-            assessmentList.innerHTML = "";
-
-            //finds cards that match each category of card
-            cardOrder.forEach(category => {
-                //finds matching cards in the specific category
-
-                const matchingCards = (cardInfo.length > 0) ? cardInfo.filter(card => card["card-type"] === "Assessment").filter((card) => card['card-category'] == category) : ('');
-                //gives random number for card within the ammount of cards in that category
-                const randomIndex = Math.floor(Math.random() * matchingCards.length);
-                selectedCards[category] = matchingCards[randomIndex];                
-            });
-            Object.values(selectedCards).forEach((element) => {
-                var assessmentItem;
-                assessmentItem = document.createElement('super-assessment-card');
-                assessmentItem.id = (element != undefined) ? 'card_'+element['card-id'] : ('');
-                //sets each pair from the JSON as an attribute in the HTML
-                var x;
-                for (x in element) {
-                    assessmentItem.setAttribute(x, element[x]);
-                }
-
-                //lastly, append to page
-                assessmentList.appendChild(assessmentItem);
-            });
-    
-        }
-
-        function addToFavouritesList(item){
-            //checks if favlist contains the card we're trying to add, so as to not add duplicated
-            if (!(favCards.some(saved => saved['card-id'] === item["card-id"]))) {
-                
-                console.log(item);
-                setFavCards([...favCards, item]);
-                const stringedList = JSON.stringify(favCards);
-                var x = localStorage.setItem("FAVOURITE_CARDS_LIST_STORE", stringedList);
-                setLoading(true);
-            }
-        }
-
-        function removeFromFavouritesList(item){
-            const found = favCards.findIndex((element) => element["card-id"] === item["card-id"]);
-            setFavCards([...favCards.splice(found, 1), item]);
-            const stringedList = JSON.stringify(favCards);
-            var x = localStorage.setItem("FAVOURITE_CARDS_LIST_STORE", stringedList);
-            setLoading(true);
-        }
-
+        import('./../../components/CardHTMLComponent/components/mission.js');
+        import('./../../components/CardHTMLComponent/components/assessment.js');
+    }, []);
 
     return (
-        <div className="game-wrapper">
-        <h1>the SUPER Assessor - Idea generator</h1>
-        <p>SUPER Assessor, a game designed for educators, is the result of a research project undertaken by the Department of Design in Trondheim. Its primary objective is to aid educators in the development of unique assessment methods. These methods are intended to create innovative ways to evaluate and grade students.</p>
-    <main>
-            <h2>Mission cards (x3)</h2>
-            <div className="card-container mission-cards" id="mission-list">
-            </div>
-
-            <h2>Assessment cards (x6)</h2>
-            <div className="card-container assessment-cards" id="assessment-list">
-            </div>
-    </main>
-
-    <aside>
-        <h2>Favourite cards</h2>
-        <ul id="favourite-cards-list">
-        </ul>
-
-        <h2>Control panel</h2>
-        <p>Click the following button to extract random cards and display them in the main area.</p>
-        <button type="button" onClick={randomizeCards}>Randomise</button>
-    </aside>
-        </div>
+        <>
+            <main>
+                <h2>Mission cards (x3)</h2>
+                <div class="card-container">
+                    <super-mission-card card-name="counteract-cheating"></super-mission-card>
+                    <super-mission-card card-name="authenticity"></super-mission-card>
+                    <super-mission-card card-name="time-management"></super-mission-card>
+                </div>
+                <h2>Assessment cards (x6)</h2>
+                <div class="card-container">
+                    <super-assessment-card card-category="assessed"></super-assessment-card>
+                    <super-assessment-card card-category="assessor"></super-assessment-card>
+                    <super-assessment-card card-category="artefact"></super-assessment-card>
+                    <super-assessment-card card-category="format"></super-assessment-card>
+                    <super-assessment-card card-category="context"></super-assessment-card>
+                    <super-assessment-card card-category="timing"></super-assessment-card>
+                </div>
+            </main>
+        </>
     );
 }
 
