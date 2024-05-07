181e8a5bea1c4dea1dc6a26f24240a2f0617b7bd
VegardTheMonkey <vegardstamadsen@gmail.com>
2024-05-07 14:06:22 +0200

    changed it back

--- frontend/src/components/AssessmentCard/index.js
+++ frontend/src/components/AssessmentCard/index.js
@@ -1,6 +1,3 @@
-
-
-
 class SuperAssessmentCard extends HTMLElement {
     constructor() {
         super();
@@ -10,12 +7,12 @@ class SuperAssessmentCard extends HTMLElement {
         `;
         this.loadAndRenderCards();
     }
-
+ 
     connectedCallback() {
         document.addEventListener('refreshCards', () => {
             this.loadAndRenderCards(); // This refreshes all cards
         });
-    
+   
         document.addEventListener('drawNewCard', (e) => {
             console.log("drawNewCard event received for category:", e.detail.category);
             if (e.detail.category === this.getAttribute('card-category') && e.detail.refresh) {
@@ -23,14 +20,14 @@ class SuperAssessmentCard extends HTMLElement {
             }
         });
     }
-
+ 
     loadAndRenderCards() {
         fetch('http://localhost:3000/api/cards')
             .then(response => response.json())
             .then(data => this.renderCards(data))
             .catch(error => console.error('Error:', error));
     }
-
+ 
     renderCards(assessmentCardData) {
         // group the cards by category
         let cardsByCategory = {};
@@ -40,7 +37,7 @@ class SuperAssessmentCard extends HTMLElement {
             }
             cardsByCategory[card["card-category"]].push(card);
         }
-
+ 
         // Map the card-category attribute to readable format if needed
         let categoryMap = {
             "assessed": "Who is assessed",
@@ -50,18 +47,18 @@ class SuperAssessmentCard extends HTMLElement {
             "context": "Context",
             "timing": "Assessment timing"
         };
-
+ 
         let category = categoryMap[this.getAttribute('card-category')];
         if (!cardsByCategory[category]) {
             console.error(`Category ${category} not found in the data`);
             return;
         }
-
+ 
         // Randomly select a card
         let selectedCard = cardsByCategory[category][Math.floor(Math.random() * cardsByCategory[category].length)];
         this.displayCard(selectedCard);
     }
-
+ 
     displayCard(card) {
         let cardHTML = `
         <style>
@@ -78,31 +75,31 @@ class SuperAssessmentCard extends HTMLElement {
             border-radius: 14px;
             background-color: white;
         }
-
+ 
         .who-is-assessed .cardCategory{
             background-color: var(--who-is-assessed-color);
         }
-
+ 
         .the-assessor .cardCategory{
             background-color: var(--the-assessor-color);
         }
-
+ 
         .assessment-artefact .cardCategory{
             background-color: var(--assessment-artefact-color);
         }
-
+ 
         .assessment-format .cardCategory{
             background-color: var(--assessment-format-color);
         }
-
+ 
         .context .cardCategory{
             background-color: var(--context-color);
         }
-        
+       
         .assessment-timing .cardCategory{
             background-color: var(--assessment-timing-color);
         }
-
+ 
         .cardCategory{
             color: white;
             font-size:18px;
@@ -114,56 +111,56 @@ class SuperAssessmentCard extends HTMLElement {
             align-content: center;
             margin-bottom:4px;
         }
-
+ 
         .card.who-is-assessed {
             border-color: var(--who-is-assessed-border-color);
         }
-
+ 
         .card.the-assessor {
             border-color: var(--the-assessor-border-color);
         }
-
+ 
         .card.assessment-artefact {
             border-color: var(--assessment-artefact-border-color);
         }
-
+ 
         .card.assessment-format {
             border-color: var(--assessment-format-border-color);
         }
-
+ 
         .card.context {
             border-color: var(--context-border-color);
         }
-
+ 
         .card.assessment-timing {
             border-color: var(--assessment-timing-border-color);
         }
-
+ 
         .cardText{
             padding: 6px;
         }
-
+ 
         .card:hover {
             border-bottom-right-radius: 50px;
             box-shadow: 80px 90px 28px -90px rgba(0,0,0,0.65);
         }
-
+ 
         .card img {
             width: 100%;
             height: 100%;
             object-fit: cover;
             display: block;
         }
-
+ 
         h2{
             font-size: 16px;
             margin: 0;
         }
-
+ 
         p {
             font-size: 12px;
         }
-
+ 
         .card-number {
             position: absolute;
             bottom: 5px;
@@ -172,12 +169,12 @@ class SuperAssessmentCard extends HTMLElement {
             font-weight: bold;
             transition: all 0.2s;
         }
-
+ 
         /* make the card number skew to create illusion of 3D effect bending */
-        .card:hover .card-number { 
+        .card:hover .card-number {
             transform: skewX(-30deg);
         }
-
+ 
         .star {
             position: absolute;
             top: 0;
@@ -188,7 +185,7 @@ class SuperAssessmentCard extends HTMLElement {
             font-size: 40px;
             visibility: hidden;
         }
-
+ 
         .star::before {
             position: absolute;
             top: 1px;
@@ -201,149 +198,14 @@ class SuperAssessmentCard extends HTMLElement {
             color: gray;
             visibility: visible;
         }
-
-                .star.active::before {
-                    color: gold;
-                }
-
-                .card-background {
-                    height: 100%;
-                    position: relative;
-                }
-
-                .card-background_topsection {
-                    height: 55%;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                }
-
-                .card-background_topsection .card-category-text {
-                    font-size: 140%;
-                    font-weight: bold;
-                    margin-top: 5px;
-                    margin: 0 30px;
-                    text-align: center;
-                }
-
-                .card-background_topsection.who-is-assessed .card-category-text, .card-background_topsection.the-assessor .card-category-text, .card-background_topsection.assessment-artefact .card-category-text {
-                    color: white;
-                }
-
-                .card-background_topsection img {
-                    width: 60px;
-                    height: 60px;
-                }
-
-                .card-background .who-is-assessed {
-                    background-color: var(--who-is-assessed-color);
-                }
-
-                .card-background .the-assessor {
-                    background-color: var(--the-assessor-color);
-                }
-
-                .card-background .assessment-artefact {
-                    background-color: var(--assessment-artefact-color);
-                }
-
-                .card-background .assessment-format {
-                    background-color: var(--assessment-format-color);
-                }
-
-                .card-background .context {
-                    background-color: var(--context-color);
-                }
-                
-                .card-background .assessment-timing {
-                    background-color: var(--assessment-timing-color);
-                }
-
-                .card-background_bottomsection {
-                    background-color: #ffe784;
-                    height: 45%;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    position: relative;
-                }
-
-                .card-background_bottomsection img {
-                    width: 40px;
-                    height: 40px;
-                }
-
-                .card-background .card-gamename {
-                    background: linear-gradient(90deg, #3d4ca0, #47a0d9);
-                    -webkit-background-clip: text;
-                    -webkit-text-fill-color: transparent;
-                    font-size: 100%;
-                }
-                .card-background .card-gamename span {
-                    font-weight: bold;
-                    font-size: 160%;
-                }
-            </style>
-
-                ${this.renderCard(selectedCard)}
-
-        `;
-        this.shadowRoot.innerHTML = missionCardHTML;
-        this.addClickEventToCards();
-    }
-
-    renderCard(card) {
-        let cardId = Number(card["card-id"]);
-        let cardCategory = card["card-category"].replace(/\s+/g, '-').toLowerCase();
-
-
-        // logo src
-        const assessmentLogoSrc =  './assets/icons/Super_assessed.png';
-
-        // card icon src
-        const whoAssessed = './assets/icons/Assessed.svg';
-        const assessor = './assets/icons/assessor.png';
-        const context = './assets/icons/Context.png';
-        const format = './assets/icons/Assessment_format.png';
-        const timing = './assets/icons/Assessment_timing.png';
-        const artefact = './assets/icons/Assessment_artefact.png';
-
-        function getImageSrc(category) {
-            switch(category) {
-                case 'who-is-assessed':
-                    return whoAssessed;
-                case 'the-assessor':
-                    return assessor;
-                case 'assessment-artefact':
-                    return artefact;
-                case 'assessment-format':
-                    return format;
-                case 'context':
-                    return context;
-                case 'assessment-timing':
-                    return timing;
-                default:
-                    return '';
-            }
+ 
+        .star.active::before {
+            color: gold;
         }
-
-        // ${getImageSrc(card['card-category'])}
-
-        return `
-            <div class="card ${cardCategory}" >
+    </style>
+            <div class="card ${card["card-category"].replace(/\s+/g, '-').toLowerCase()}" >
                 <button class="star" data-card-id="${card['card-id']}" data-card-name="${card['card-name']}"></button>
-                <div class="card-background" style="display: none;" data-card-category="${card['card-category']}">
-                    <div class="card-background_topsection ${cardCategory}">
-                        <img src="${getImageSrc(cardCategory)}" class="card-background_card-logo" alt="${card["card-category"]}"></img>
-                        <div class="card-category-text">${card['card-category']}</div>
-                    </div>
-                    <div class="card-background_bottomsection">
-                        <img src="${assessmentLogoSrc}" alt="Assessment card"></img>
-                        <div class="card-gamename"><span>SUPER</span><br>ASSESSOR</div>
-                    </div>
-                </div>
+                <img class="card-image" style="display: none;" src="./assets/cards-png/SUPER cards poker size ${"061123" + (card['card-id'] * 2)}.png">
                 <div class="card-content">
                     <div class="cardCategory">${card["card-category"]}</div>
                     <div class="cardText">
@@ -359,51 +221,29 @@ class SuperAssessmentCard extends HTMLElement {
         this.shadowRoot.innerHTML = cardHTML;
         this.addClickEventToCards();
     }
-
+ 
     addClickEventToCards() {
         const cards = this.shadowRoot.querySelectorAll('.card');
         cards.forEach(card => {
             card.addEventListener('click', () => {
-                const cardContentElement = card.querySelector('.card-content');
-                const imageElement = card.querySelector('.card-background');
-                const cardId = imageElement.getAttribute('data-card-id');
-
-
-                // find the image for back of the card
-                const imageBackSrc = `./assets/cards-png/SUPER cards poker size ${"061123" + (cardId * 2)}.png`;
-
-                // Check if the card is currently displaying the text content or the back image and update accordingly
-                if (cardContentElement.style.display === 'none') {
-                    // If the text content is hidden, show it and hide the back image
-                    cardContentElement.style.display = 'block';
-                    imageElement.style.display = 'none';
-                } else {
-                    // If the text content is shown, hide it and show the back image
-                    cardContentElement.style.display = 'none';
-                    imageElement.style.display = 'block';
-                    imageElement.setAttribute('src', imageBackSrc);
+                const cardContent = card.querySelector('.card-content');
+                const cardImage = card.querySelector('.card-image');
+                const cardId = card.getAttribute('data-card-id');
+   
+                // Compute image source based on cardId
+                if (cardId && !isNaN(cardId)) {
+                    const imageBackSrc = `./assets/cards-png/SUPER cards poker size 061123${cardId * 2}.png`;
+                    cardImage.src = imageBackSrc;
                 }
-            });
- 
-            const starButton = card.querySelector('.star');
-            starButton.addEventListener('click', (event) => {
-                event.stopPropagation();
-
-                // Toggle the 'active' class on the star button
-                starButton.classList.toggle('active');
-
-                // Dispatch different events based on whether the star is active or not
-                let customEventName;
-                let customEventDetail;
-                if (starButton.classList.contains('active')) {
-                    customEventName = 'clickStar';
-                    customEventDetail = { 
-                        cardName: starButton.getAttribute('data-card-name')
-                    };
+   
+                // Toggle display of card content and image
+                if (cardContent.style.display === 'none') {
+                    cardContent.style.display = 'block';
+                    cardImage.style.display = 'none';
                 } else {
                     cardContent.style.display = 'none';
                     cardImage.style.display = 'block';
-    
+   
                     // Dispatch event only if flipping to show the image (back of the card)
                     document.dispatchEvent(new CustomEvent('cardFlipped', {
                         detail: {
@@ -415,7 +255,7 @@ class SuperAssessmentCard extends HTMLElement {
             });
         });
     }
-    
+   
 }
-
+ 
 customElements.define('super-assessment-card', SuperAssessmentCard);
--- frontend/src/components/MissionCard/index.js
+++ frontend/src/components/MissionCard/index.js
@@ -6,13 +6,13 @@ class SuperMissionCard extends HTMLElement {
         this.cardId = this.getAttribute('card-id');  // Unique identifier for each card
         this.loadAndRenderCards();
     }
-
+ 
     connectedCallback() {
         document.addEventListener('refreshCards', () => {
             console.log('Refreshing all mission cards');
             this.loadAndRenderCards();
         });
-
+ 
         document.addEventListener('drawNewCard', (event) => {
             console.log(`Received drawNewCard event for cardId: ${event.detail.cardId}, this cardId: ${this.cardId}`);
             if (event.detail.cardId === this.cardId) {
@@ -21,152 +21,164 @@ class SuperMissionCard extends HTMLElement {
             }
         });
     }
-
+ 
     loadAndRenderCards() {
         fetch("http://localhost:3000/api/cards")
             .then(response => response.json())
-            .then(data => this.renderCards(data))
-            .catch(error => console.error('Error:', error));
+            .then(data => {
+                const missionCardData = data.filter(card => card['card-type'] === 'Mission');
+                console.log(`Mission cards fetched: ${missionCardData.length}`);
+                if (missionCardData.length > 0) {
+                    this.renderRandomCard(missionCardData);
+                } else {
+                    console.log('No mission cards available to display.');
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching mission cards:", error);
+            });
     }
-
-    renderCards(missionCardData) {
-        // get the card name from the HTML attribute
-        let cardName = this.getAttribute('card-name').replace(/-/g, ' ').toLowerCase();
-        let selectedCard = missionCardData.find(card => card['card-name'].toLowerCase() === cardName);
-
-        if (!selectedCard) {
-            console.error(`Card with name "${cardName}" not found.`);
-            return;
-        }
-        let missionCardHTML = `
-            <style>
-                .card {
-                    width: 220px;
-                    aspect-ratio: 5 / 7;
-                    border: 11px solid #FCE18F;
-                    cursor: pointer; 
-                    position: relative; 
-                    transition: all 0.2s;
-                    overflow: hidden;
-                    user-select: none;
-                    box-sizing: border-box;
-                    border-radius: 14px;
-                    background-color: white;
-                }
-
-                .card-header {
-                    font-size: 18px;
-                    font-weight: bold;
-                    margin: 10px 20px 0;
-                }
-                .card-body {
-                    margin: 10px 20px;
-                }
-                .card img {
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-                    display: block; 
-                }
-                .card:hover {
-                    border-bottom-right-radius: 50px;
-                    box-shadow: 80px 90px 28px -90px rgba(0,0,0,0.45);
-                }
-                .card-background {
-                    height: 100%;
-                    position: relative;
-                }
-                .card-background_topsection {
-                    height: 55%;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                }
-                .card-background_topsection .card-category-text {
-                    font-size: 165%;
-                    font-weight: bold;
-                    margin-top: 5px;
-                    margin: 0 30px;
-                    text-align: center;
-                    color: #3f90ce;
-                }
-                .card-background_bottomsection {
-                    background-color: #ffe784;
-                    height: 45%;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    position: relative;
-                }
-
-                .card-background_bottomsection img {
-                    width: 40px;
-                    height: 40px;
-                }
-                .card-background .card-gamename {
-                    background: linear-gradient(90deg, #3d4ca0, #47a0d9);
-                    -webkit-background-clip: text;
-                    -webkit-text-fill-color: transparent;
-                    font-size: 100%;
-                }
-                .card-background .card-gamename span {
-                    font-weight: bold;
-                    font-size: 160%;
-                }
-            </style>
-            <div class="card-container">
-            
-                <div class="card" data-card-image="${selectedCard["card-image"]}">
-                    <div class="card-content">
-                        <div class="card-header">${selectedCard["card-name"]}</div>
-                        <div class="card-body">${selectedCard["card-description"].replace(/\n/g, '<br>')}</div>
-                    </div>
+ 
+    renderRandomCard(missionCardData) {
+        const selectedCard = missionCardData[Math.floor(Math.random() * missionCardData.length)];
+        console.log(`Rendering card: ${selectedCard['card-name']}`);
+        this.renderCard(selectedCard);
+    }
+ 
+    renderCard(card) {
+        let cardHTML = `
+        <style>
+            .card {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                width: 220px;
+                aspect-ratio: 5 / 7;
+                border: 11px solid #FCE18F;
+                cursor: pointer;
+                position: relative;
+                transition: all 0.2s;
+                overflow: hidden;
+                user-select: none;
+                box-sizing: border-box;
+                border-radius: 14px;
+                background-color: white;
+            }
+            .card-header {
+                font-size: 18px;
+                font-weight: bold;
+                margin: 10px 20px 0;
+            }
+            .card-body {
+                margin: 10px 20px;
+            }
+            .card img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                display: none;
+            }
+            .card:hover {
+                border-bottom-right-radius: 50px;
+                box-shadow: 80px 90px 28px -90px rgba(0,0,0,0.45);
+            }
+            button.replace-button {
+                margin-top: 10px;
+                padding: 5px 10px;
+                font-size: 16px;
+                background-color: #4CAF50;
+                color: white;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+            }
+            .card-background {
+                height: 100%;
+                position: relative;
+            }
+            .card-background_topsection {
+                height: 55%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+            .card-background_topsection .card-category-text {
+                font-size: 165%;
+                font-weight: bold;
+                margin-top: 5px;
+                margin: 0 30px;
+                text-align: center;
+                color: #3f90ce;
+            }
+            .card-background_bottomsection {
+                background-color: #ffe784;
+                height: 45%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                position: relative;
+            }
+            .card-background_bottomsection img {
+                width: 40px;
+                height: 40px;
+                display: block;
+            }
+            .card-background .card-gamename {
+                background: linear-gradient(90deg, #3d4ca0, #47a0d9);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                font-size: 100%;
+            }
+            .card-background .card-gamename span {
+                font-weight: bold;
+                font-size: 160%;
+            }
+        </style>
+        <div class="card">
+            <div class="card-content">
+                <div class="card-header">${card["card-name"]}</div>
+                <div class="card-body">${card["card-description"].replace(/\n/g, "<br>")}</div>
+            </div>
+            <div class="card-background" style="display: none;">
+                <div class="card-background_topsection">
+                    <div class="card-category-text">Mission</div>
+                </div>
+                <div class="card-background_bottomsection">
+                    <img src="../../assets/icons/Super_assessed.png" alt="Assessment card"></img>
+                    <div class="card-gamename"><span>SUPER</span><br>ASSESSOR</div>
                 </div>
             </div>
+            <button class="replace-button">Replace</button>
+        </div>
         `;
         this.shadowRoot.innerHTML = cardHTML;
         this.addClickEventToCard();
+        import("../../assets/icons/Super_assessed.png");
     }
-
-    addClickEventToCards() {
-        const cards = this.shadowRoot.querySelectorAll('.card');
-        cards.forEach(card => {
-            card.addEventListener('click', () => {
-                const content = card.querySelector('.card-content');
-                const assessmentLogoSrc =  './assets/icons/Super_assessed.png';
-                // Check if the card is currently displaying the image or the text
-                if (content) {
-                    // Card is currently showing text, switch to image
-                    const imageHtml = `
-                        <div class="card-background" >
-                            <div class="card-background_topsection ">
-
-                                <div class="card-category-text">Mission</div>
-                            </div>
-                            <div class="card-background_bottomsection">
-                                <img src="${assessmentLogoSrc}" alt="Assessment card"></img>
-                                <div class="card-gamename"><span>SUPER</span><br>ASSESSOR</div>
-                            </div>
-                        </div>
-                    `;
-                    card.innerHTML = imageHtml;
-                } else {
-                    // Card is currently showing image, revert back to original text content
-                    const originalContent = `
-                        <div class="card-content">
-                            <div class="card-header">${card.dataset.cardName}</div>
-                            <div class="card-body">${card.dataset.cardDescription}</div>
-                        </div>`;
-                    card.innerHTML = originalContent;
-                }
-            });
-            // Store the card's original content in dataset for easy revert
-            card.dataset.cardName = card.querySelector('.card-header').innerText;
-            card.dataset.cardDescription = card.querySelector('.card-body').innerHTML;
+ 
+    addClickEventToCard() {
+        const card = this.shadowRoot.querySelector(".card");
+        const imageElement = card.querySelector(".card-background");
+        const contentElement = card.querySelector(".card-content");
+        const replaceButton = card.querySelector(".replace-button");
+   
+        card.addEventListener("click", () => {
+            if (imageElement.style.display === 'block') {
+                imageElement.style.display = 'none';
+                contentElement.style.display = 'block';
+            } else {
+                imageElement.style.display = 'block';
+                contentElement.style.display = 'none';
+            }
+        });
+   
+        replaceButton.addEventListener("click", (e) => {
+            e.stopPropagation();  // Prevent the card click event from firing
+            this.loadAndRenderCards();  // Reload a new mission card
         });
     }
 }
-
+ 
 customElements.define("super-mission-card", SuperMissionCard);
