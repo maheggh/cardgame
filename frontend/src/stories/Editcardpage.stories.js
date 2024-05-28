4bb5963d0f33406dc8f5aed97f75353693007bb3
maheggh <111765195+maheggh@users.noreply.github.com>
2024-05-28 12:04:31 +0200

    ,

--- frontend/src/stories/Editcardpage.stories.js
+++ frontend/src/stories/Editcardpage.stories.js
@@ -1,36 +1,9 @@
 import React from 'react';
-import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import EditCardPage from '../../src/pages/EditCardPage/index';
 import { within, userEvent } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-import { getSingleCard, updateCard, deleteCard } from '../../src/API/cards';
-import jest from 'jest-mock';
-
-// Manual mock implementations of the API functions
-const mockGetSingleCard = jest.fn();
-const mockUpdateCard = jest.fn();
-const mockDeleteCard = jest.fn();
-
-jest.mock('../../src/API/cards', () => ({
-  getSingleCard: mockGetSingleCard,
-  updateCard: mockUpdateCard,
-  deleteCard: mockDeleteCard,
-}));
-
-// Mock data for the card
-const mockCardData = {
-  'card-id': '1',
-  'card-type': 'type1',
-  'card-category': 'category1',
-  'card-name': 'Card Name',
-  'card-description': 'Card Description',
-  'card-details': 'Card Details',
-};
-
-// Mock implementations of the API functions
-mockGetSingleCard.mockResolvedValue(mockCardData);
-mockUpdateCard.mockResolvedValue(mockCardData);
-mockDeleteCard.mockResolvedValue({});
+import { mockGetSingleCard, mockUpdateCard, mockDeleteCard } from './mocks';
 
 export default {
   title: 'Example/EditCardPage',
@@ -51,7 +24,9 @@ export default {
     // Define argTypes if necessary
   },
   args: {
-    // Provide default args if needed
+    getSingleCard: mockGetSingleCard,
+    updateCard: mockUpdateCard,
+    deleteCard: mockDeleteCard,
   },
 };
 
@@ -62,7 +37,6 @@ Default.args = {
   // Add default args for the EditCardPage component if any
 };
 
-// Example of an interaction test
 export const InteractionTest = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
---
+++ frontend/src/stories/mocks.js
@@ -0,0 +1,16 @@
+// Mock data for the card
+const exampleCardData = {
+    'card-id': '1',
+    'card-type': 'type1',
+    'card-category': 'category1',
+    'card-name': 'Card Name',
+    'card-description': 'Card Description',
+    'card-details': 'Card Details',
+  };
+  
+  export const mockGetSingleCard = async () => exampleCardData;
+  
+  export const mockUpdateCard = async () => ({});
+  
+  export const mockDeleteCard = async () => ({});
+  
